c5bf9b0051c6a791d6da62e07dadab3e70027990
YaelMetz1 <yaelmetz21@gmail.com>
2024-03-08 00:20:10 +0200

    homePage spaces

--- client/src/pages/homePage/HomePage.tsx
+++ client/src/pages/homePage/HomePage.tsx
@@ -1,32 +1,27 @@
 import React, { useContext } from "react";
 import { UserContext } from "../../context/UserContext";
-import { Card, CardContent, Typography, CardActions, Button } from "@mui/material";
+import {
+  Card,
+  CardContent,
+  Typography
+} from "@mui/material";
 
 export default function HomePage() {
-
   const { user, setUser } = useContext(UserContext);
 
-
-
-    return (
-      <Card sx={{ minWidth: 275 }}>
-        <CardContent>
+  return (
+    <Card sx={{ minWidth: 275 }}>
+      <CardContent>
         <Typography variant="h4" component="div">
           Name: {user?.name}
         </Typography>
-          <Typography variant="body2">
-           Email: {user?.email}
-          </Typography>
-          <Typography variant="body2">
-           Phone Number: {user?.phoneNumber}
-          </Typography>
-          <Typography variant="body2">
-           Job Title: {user?.jobTitle}
-          </Typography>
-          <Typography variant="body2">
-           Salary: {user?.salary}
-          </Typography>
-        </CardContent>
-      </Card>
-    );
-  }
+        <Typography variant="body2">Email: {user?.email}</Typography>
+        <Typography variant="body2">
+          Phone Number: {user?.phoneNumber}
+        </Typography>
+        <Typography variant="body2">Job Title: {user?.jobTitle}</Typography>
+        <Typography variant="body2">Salary: {user?.salary}</Typography>
+      </CardContent>
+    </Card>
+  );
+}
